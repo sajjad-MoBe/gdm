51011035522bebbc14a10567082d904f4c3c973d
Sajjad <sajjad@Sajjads-MacBook-Pro.local>
2025-03-21 03:18:50 +0330

    debug pause/resume download

--- internal/tui/ui.go
+++ internal/tui/ui.go
@@ -195,7 +195,6 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.removeQueue()
 			}
 		case "p": // Pause/Resume selected download
-			fmt.Println(m.currentTab == tabDownloads)
 			if m.currentTab == tabDownloads {
 				m.togglePauseDownload()
 			}
