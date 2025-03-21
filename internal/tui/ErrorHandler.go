1cb5aafb98cea4104ff03dfcab6d831e52c06676
Mohammad <mohammadmohammadbeigi1381@gmail.com>
2025-03-21 13:26:13 +0330

    Retries Errors Added!

--- internal/tui/ErrorHandler.go
+++ internal/tui/ErrorHandler.go
@@ -31,16 +31,39 @@ func (m *Model) handleConcurrentError() {
 	m.setupsAfterErrorForQueues()
 }
 
+func (m *Model) handleRetriesError() {
+	m.errorMessage = "Invalid Max Retries Input!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
 func (m *Model) handleAllErrors() {
 	// Logic for when all errors are present
+	m.errorMessage = "Max Concurrent, Max Bandwidth, Max Retries ,and Time inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleBWAndConcurrentAndTimeErrors() {
 	m.errorMessage = "Max Concurrent, Max Bandwidth, and Time inputs are invalid!"
 	m.confirmationMessage = ""
 	m.setupsAfterErrorForQueues()
 }
 
-func (m *Model) handleConcurrentAndBWAndRetriesErrors() {
-	// Logic for concurrent + bandwidth errors
-	m.errorMessage = "Max Concurrent and Max Bandwidth and Max Retries Per Download inputs are invalid!"
+func (m *Model) handleRetriesAndConcurrentAndTimeErrors() {
+	m.errorMessage = "Max Concurrent, Max Retries, and Time inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleRetriesAndConcurrentAndBWErrors() {
+	m.errorMessage = "Max Concurrent, Max Bandwidth, and Max Retries inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleRetriesAndBWAndTimeErrors() {
+	m.errorMessage = "Max Retries, Max Bandwidth, and Time inputs are invalid!"
 	m.confirmationMessage = ""
 	m.setupsAfterErrorForQueues()
 }
@@ -52,9 +75,36 @@ func (m *Model) handleBWAndTimeErrors() {
 	m.setupsAfterErrorForQueues()
 }
 
+func (m *Model) handleBWAndConcurrentErrors() {
+	m.errorMessage = "Max Bandwidth and Max Concurrent inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
 func (m *Model) handleConcurrentAndTimeErrors() {
 	// Logic for concurrent + time errors
 	m.errorMessage = "Max Concurrent and Time format inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleConcurrentAndRetriesErrors() {
+	// Logic for concurrent + time errors
+	m.errorMessage = "Max Concurrent and Max Retries inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleTimeAndRetriesErrors() {
+	// Logic for concurrent + time errors
+	m.errorMessage = "Max Retries and Time format inputs are invalid!"
+	m.confirmationMessage = ""
+	m.setupsAfterErrorForQueues()
+}
+
+func (m *Model) handleBWAndRetriesErrors() {
+	// Logic for concurrent + time errors
+	m.errorMessage = "Max BandWidth and Max Retries inputs are invalid!"
 	m.confirmationMessage = ""
 	m.setupsAfterErrorForQueues()
 }
@@ -121,17 +171,40 @@ func (m *Model) CheckErrorCodes() int {
 	if !regForHHMMFormat.MatchString(m.activeStartTimeInput.Value()) || !regForHHMMFormat.MatchString(m.activeEndTimeInput.Value()) {
 		timeError = true
 	}
-	if timeError && concurrentError && bwError {
+
+	if timeError && concurrentError && bwError && retriesError {
 		m.handleAllErrors()
+		return 1
+	} else if timeError && concurrentError && bwError {
+		m.handleBWAndConcurrentAndTimeErrors()
+
+		return 1
+	} else if timeError && concurrentError && retriesError {
+		m.handleRetriesAndConcurrentAndTimeErrors()
+		return 1
+	} else if retriesError && concurrentError && bwError {
+		m.handleRetriesAndConcurrentAndBWErrors()
+		return 1
+	} else if timeError && bwError && retriesError {
+		m.handleRetriesAndBWAndTimeErrors()
 		return 1
 	} else if timeError && concurrentError {
 		m.handleConcurrentAndTimeErrors()
 		return 1
-	} else if bwError && concurrentError && retriesError {
-		m.handleConcurrentAndBWAndRetriesErrors()
+	} else if bwError && retriesError {
+		m.handleBWAndRetriesErrors()
 		return 1
-	} else if bwError && timeError {
+	} else if timeError && bwError {
 		m.handleBWAndTimeErrors()
+		return 1
+	} else if timeError && retriesError {
+		m.handleTimeAndRetriesErrors()
+		return 1
+	} else if bwError && concurrentError {
+		m.handleBWAndConcurrentErrors()
+		return 1
+	} else if retriesError && concurrentError {
+		m.handleConcurrentAndRetriesErrors()
 		return 1
 	} else if timeError {
 		m.handleTimeError()
@@ -141,6 +214,9 @@ func (m *Model) CheckErrorCodes() int {
 		return 1
 	} else if concurrentError {
 		m.handleConcurrentError()
+		return 1
+	} else if retriesError {
+		m.handleRetriesError()
 		return 1
 	}
 	return 0
